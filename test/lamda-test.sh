a3786d29127fa7270fc1a66f9d08cd2986386f45
slalom <ubuntu@ip-172-26-4-180.us-west-2.compute.internal>
2023-07-13 20:24:14 +0000

    edit test script

--- test/lamda-test.sh
+++ test/lamda-test.sh
@@ -1,2 +1,2 @@
-RESPONSE = `aws lambda invoke --function-name beta-lambda --cli-binary-format raw-in-base64-out --payload '{ "key": "value" }' repsonse.json`
+export RESPONSE=`aws lambda invoke --function-name beta-lambda --cli-binary-format raw-in-base64-out --payload '{ "key": "value" }' repsonse.json`
 echo RESPONSE is $REPONSE
